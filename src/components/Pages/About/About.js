28cd028260939050af26a4b14dc977d859b6d0fb
zubayer-salehin <zubaersalehin@gmail.com>
2022-04-18 20:22:58 +0600

    about,home,checkout page done

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import "./About.css";
 
 const About = () => {
     return (
-        <div className='about'>
-            About Us
-        </div>
+        <Container>
+            <div className='about mt-4'>
+                <h3>Name : Zubayer Salehin</h3>
+                <h6>Goal :</h6>
+                <p>My name is zubayer salehin.I will learn web development.I am daily learn,practice and working 10-12 hour approx. Web developers have two main career paths to choose from, depending on work style and level of experience. You might opt for a stable, salaried position, or enjoy more autonomy as an independent contractor.Programming skills will always be in demand. Complement your coding prowess with a strong work ethic, and you’ll build a career that comes with high pay and even higher job satisfaction.</p>
+            </div>
+        </Container>
     );
 };
 
